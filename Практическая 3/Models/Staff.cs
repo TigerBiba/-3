74b7d62b3053ee98d358bd29fb5a50ce46eb01d5
Mickey <artplay1995@gmail.com>
2025-01-16 10:53:48 +0700

    тут тоже особо ничего нет, ну фоточки добавились

--- Практическая 3/Models/Staff.cs
+++ Практическая 3/Models/Staff.cs
@@ -29,6 +29,7 @@ namespace Практическая_3.Models
         public Nullable<int> ID_login { get; set; }
         public string firstname { get; set; }
         public string secondname { get; set; }
+        public string photo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Departament> Departament { get; set; }
